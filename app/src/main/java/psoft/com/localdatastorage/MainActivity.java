926bfd70eb586eea09d573d4c58b7996301c22fa
psoftbd19 <psoftbd19@gmail.com>
2019-01-22 21:28:46 +0600

    Collections data

--- app/src/main/java/psoft/com/localdatastorage/MainActivity.java
+++ app/src/main/java/psoft/com/localdatastorage/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import psoft.com.localdatastorage.model.DataItem;
+import psoft.com.localdatastorage.sample.SampleDataProvider;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvOutPut;
 
+    List<DataItem> dataItemList = SampleDataProvider.itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +25,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tvOutPut =(TextView) findViewById(R.id.outPut);
 
-        DataItem item = new DataItem("","Shirt","Yellow","L",77,781.67,"shirt.jpg");
+        tvOutPut.setText("");
 
-        tvOutPut.setText(item.toString());
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getItemName().compareTo(o2.getItemName());
+            }
+        });
+
+        for (DataItem item:
+             dataItemList) {
+            String name = item.getItemName();
+            tvOutPut.append(name+"\n");
+
+        }
 
     }
 }
