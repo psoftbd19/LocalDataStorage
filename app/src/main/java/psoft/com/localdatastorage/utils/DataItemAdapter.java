1b9a533b1c6b1a940a4476969ef2bd239c12cb3b
psoftbd19 <psoftbd19@gmail.com>
2019-02-16 18:16:05 +0600

    ImageAssets

--- app/src/main/java/psoft/com/localdatastorage/utils/DataItemAdapter.java
+++ app/src/main/java/psoft/com/localdatastorage/utils/DataItemAdapter.java
@@ -1,6 +1,7 @@
 package psoft.com.localdatastorage.utils;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,6 +11,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import psoft.com.localdatastorage.R;
@@ -42,8 +45,16 @@ public class DataItemAdapter extends ArrayAdapter<DataItem> {
         DataItem dataItem = mItemList.get(position);
 
         tvItemName.setText(dataItem.getItemName());
-        imageView.setImageResource(R.drawable.apple_pie);
-
+//        imageView.setImageResource(R.drawable.apple_pie);
+        String imageFile=dataItem.getImage();
+        InputStream inputStream= null;
+        try {
+            inputStream = getContext().getAssets().open(imageFile);
+            Drawable d= Drawable.createFromStream(inputStream,null);
+            imageView.setImageDrawable(d);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         return convertView;
