6375de5ccf78a19ea5e72a68b0973759f10d79d1
psoftbd19 <psoftbd19@gmail.com>
2019-02-16 18:10:36 +0600

    RecyclerView

---
+++ app/src/main/java/psoft/com/localdatastorage/DataItemAdapter.java
@@ -0,0 +1,69 @@
+package psoft.com.localdatastorage;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import psoft.com.localdatastorage.model.DataItem;
+
+public class DataItemAdapter extends RecyclerView.Adapter<DataItemAdapter.ViewHolder> {
+
+    private List<DataItem> mItems;
+    private Context mContext;
+
+    public DataItemAdapter(Context context, List<DataItem> items) {
+        this.mContext = context;
+        this.mItems = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View itemView = inflater.inflate(R.layout.list_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        DataItem item = mItems.get(position);
+
+        try {
+            holder.tvName.setText(item.getItemName());
+            String imageFile = item.getImage();
+            InputStream inputStream = mContext.getAssets().open(imageFile);
+            Drawable d = Drawable.createFromStream(inputStream, null);
+            holder.imageView.setImageDrawable(d);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvName;
+        public ImageView imageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvName = (TextView) itemView.findViewById(R.id.tvItemName);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
+}
